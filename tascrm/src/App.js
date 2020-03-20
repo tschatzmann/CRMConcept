f99c521412bea99be7347ac627ae2f1ece044585
tschatzmann <tarrta2002@gmail.com>
2020-03-20 17:08:04 -0400

    first set of file

---
+++ tascrm/src/App.js
@@ -0,0 +1,80 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+        <h1>this is a CRM test</h1>
+        
+
+      <body>
+        <div class="card">
+        <div class="card-header">
+          header
+        </div>
+        <div class="list-group">
+        <a  class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">03/20/20 Call Contract for Wix</h5>
+          </div>
+          <p class="mb-1">Billto 12345 James Jones</p>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">03/20/20 Call Contract for Wix</h5>
+            </div>
+            <p class="mb-1">Billto 12345 James Jones</p>
+            <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Dropdown button
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">03/20/20 Call Contract for Wix</h5>
+            </div>
+            <p class="mb-1">Billto 12345 James Jones</p>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">03/20/20 Call Contract for Wix</h5>
+            </div>
+            <p class="mb-1">Billto 12345 James Jones</p>
+          </a>          
+          <a href="#" class="list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">03/20/20 Call Contract for Wix</h5>
+            </div>
+            <p class="mb-1">Billto 12345 James Jones</p>
+          </a>          
+          <a href="#" class="list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">03/20/20 Call Contract for Wix</h5>
+            </div>
+            <p class="mb-1">Billto 12345 James Jones</p>
+          </a>
+      </div>
+      </div>
+      </body>
+      </div>
+
+  );
+}
+
+export default App;
